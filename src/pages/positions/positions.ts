12d12f5e88d6922995df1d974924243ca135e9c9
Santiago Moral <santaigomoral@gmail.com>
2017-10-05 01:28:58 -0300

    added loagind to ajax requests

--- src/pages/positions/positions.ts
+++ src/pages/positions/positions.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { HTTP } from '@ionic-native/http';
+import { LoadingController } from 'ionic-angular';
 
 @Component({
   selector: 'page-positions',
@@ -10,15 +11,26 @@ export class PositionsPage {
   html;
   url;
   tableClass;
+  title;
   
-  constructor(public navCtrl: NavController, public navParams: NavParams, public http: HTTP) {
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams, public http: HTTP,
+    public loadingCtrl: LoadingController
+  ) {
     this.url = navParams.get('url');
     this.tableClass = navParams.get('tableClass');
+    this.title = navParams.get('title');
+    const loading = this.loadingCtrl.create({
+      content: 'Cargando '+this.title+'...'
+    });
+    loading.present();
     this.http.get(this.url,{},{}).then(
       data => {
         let parser = new DOMParser();
         let parsedHtml = parser.parseFromString(data.data, 'text/html');
         this.html = parsedHtml.getElementsByClassName(this.tableClass)[0].outerHTML;
+        loading.dismiss();
       }).catch(error => {
         console.log(error.status);
         console.log(error.error); // error message as string
